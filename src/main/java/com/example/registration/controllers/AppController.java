98dcf4997c8e64e4c4a364f273c9baa2b0f0bd0f
lukmanleon <lukman.leon@gmail.com>
2021-02-18 20:23:03 +0100

    - added index HTML file for application index and signup_form - added the application controller - added bootsrap dependencies nad commented out the ones for spring security

---
+++ src/main/java/com/example/registration/controllers/AppController.java
@@ -0,0 +1,22 @@
+package com.example.registration.controllers;
+
+import com.example.registration.entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("")
+    public String viewHomepage(){
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+}
