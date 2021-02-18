e5f49c9dca2cada8193001184f7852e6659bb098
lukmanleon <lukman.leon@gmail.com>
2021-02-18 19:25:44 +0100

    - changed the application.properties to application.yml and added the connection to the database - added the user entity, repository and test

---
+++ src/test/java/com/example/registration/UserRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.registration;
+
+import com.example.registration.entities.User;
+import com.example.registration.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateUser(){
+        User user = new User();
+        user.setEmail("leon@test.com");
+        user.setPassword("password");
+        user.setFirstName("Leon");
+        user.setLastName("Lukman");
+
+        User savedUser = userRepo.save(user);
+        User existUser = entityManager.find(User.class, savedUser.getId());
+        assert(existUser.getEmail().equals(user.getEmail()));
+    }
+}
